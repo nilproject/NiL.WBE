199320ffdc164909cf73f58100d4d9ee922ff152
nilproject <MiTiA-dzen1922>
2014-02-27 00:47:05 +0700

    nginx stop

--- NiL.WBE/Program.cs
+++ NiL.WBE/Program.cs
@@ -12,20 +12,6 @@ namespace NiL.WBE
         static void Main(string[] args)
         {
             System.Diagnostics.Process.Start("nginx", "-s stop");
-            System.Threading.ThreadPool.QueueUserWorkItem((o) =>
-            {
-                try
-                {
-                    var server0 = new HttpServer(new Logic.DummyLogic(), 80);
-                    server0.Log += (s, e) => { Console.WriteLine(e.Text); };
-                    server0.Run();
-                }
-                catch
-                {
-
-                }
-            });
-
             try
             {
                 var server = new HttpServer(new Logic.DummyLogic(), 8080);
